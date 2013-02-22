d552b7777b2841d570daaebd72b475dcb9990bae
sirochoco <sirochoco@gmail.com>
2013-02-22 10:16:17 +0900

    fromMac

---
+++ Assets/Script/CellColor.js
@@ -0,0 +1,37 @@
+﻿static var selectedGameObject:GameObject;
+private var toggleColor:boolean;
+
+function Start () {
+toggleColor=true;
+}
+
+function Update () {
+
+//ステータス切り替えで色を変更する
+	if(toggleColor==true){
+		this.gameObject.renderer.material.color=Color.red;
+		}else if(toggleColor==false){
+		this.gameObject.renderer.material.color=Color.cyan;
+	}
+}
+
+function OnMouseDown(){
+	var ray:Ray;
+	var hit:RaycastHit;
+	//マウスカーソルの位置からカメラに表示された画面の奥方向へのRayを取得
+	ray=Camera.main.ScreenPointToRay(Input.mousePosition);
+	//取得したRayの値、Rayと衝突したオブジェクトの情報、Rayの距離
+		if(Physics.Raycast(ray,hit,100)){
+			selectedGameObject=hit.collider.gameObject;
+			
+			//選択された（自身）オブジェクトのステータスを切り替える
+			if(toggleColor==true){
+				toggleColor=false;
+			}else{
+		toggleColor=true;
+	}
+			//Debug.Log(selectedGameObject.name);
+		}else{
+		selectedGameObject=null;
+		}
+}
---
+++ Assets/Script/CellGenerator.js
@@ -0,0 +1,22 @@
+﻿var cellPrefab:GameObject;
+var cellArray:Array;
+var cellSize:int=10;
+var cellLength:int=6;
+
+private var vec:Vector3;
+private var i:int;
+private var j:int;
+
+
+function Start () {
+	for (i=0;i<=cellLength;i++){
+		for(j=0;j<=cellLength;j++){
+		trans=Vector3(i*cellSize,j*cellSize,0);
+			Instantiate(cellPrefab,trans,Quaternion.Euler(-90,0,0));
+				}
+			}	
+}
+
+function Update () {
+
+}
